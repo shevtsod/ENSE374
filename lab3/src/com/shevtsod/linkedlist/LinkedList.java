8b9bcbe5ad65101a083fe5ceaff2befa6be86202
shevtsod <daniel.shevtsov@gmail.com>
2016-10-05 16:26:27 -0600

    add LinkedList methods

--- lab3/src/com/shevtsod/linkedlist/LinkedList.java
+++ lab3/src/com/shevtsod/linkedlist/LinkedList.java
@@ -1,11 +1,15 @@
 package com.shevtsod.linkedlist;
 
-public class LinkedList {	
+public class LinkedList {
+	private ListElement head, tail, le;
+	private int _nElements;
+	
+	
 	/**
 	 * Construct an empty linked list.
 	 */
 	public LinkedList() {
-		
+		le = head = tail = null;
 	}
 	
 	/**
@@ -14,7 +18,18 @@ public class LinkedList {
 	 *   next ListElement to be added to the list
 	 */
 	public void addElement(ListElement le) {
+		//Create a new node and deep copy from passed-in node.
+		this.le = new ListElement();
+		this.le.setData(le.getData());
 		
+		//If no head, set this node to head and tail
+		if(head == null) {
+			this.head = this.tail = le;
+		} else {
+			this.le = new ListElement();
+			this.tail = le;
+		}
+			
 	}
 	
 	/**
